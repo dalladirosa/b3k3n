12d31b9ea1673b101264b614160dc195711d2c37
dalladirosa <dalladirosa@gmail.com>
2022-10-24 01:47:14 +0900

    chore: add title in home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -17,21 +17,26 @@ function HomePage() {
   const { data: categories } = useQuery(['categories'], getCategories);
 
   return (
-    <div className="grid gap-4 grid-cols-2 my-auto">
-      {categories?.map((category, index) => (
-        <Link
-          to={`/category/${category.id}?name=${encodeURIComponent(
-            category.name
-          )}`}
-          key={category.id}
-          className={`${
-            COLORS[index]
-          } p-4 rounded-lg text-[#333] text-base font-semibold w-full cursor-pointer h-[100px] hover:outline outline-offset-2 outline-2 outline-violet-500
+    <div className="my-auto">
+      <h1 className="text-[#333] text-2xl font-semibold mb-6">
+        Choose category
+      </h1>
+      <div className="grid gap-4 grid-cols-2">
+        {categories?.map((category, index) => (
+          <Link
+            to={`/category/${category.id}?name=${encodeURIComponent(
+              category.name
+            )}`}
+            key={category.id}
+            className={`${
+              COLORS[index]
+            } p-4 rounded-lg text-[#333] text-base font-semibold w-full cursor-pointer h-[100px] hover:outline outline-offset-2 outline-2 outline-violet-500
            ${index === categories.length - 1 ? 'col-span-2' : ''}`}
-        >
-          {category.name}
-        </Link>
-      ))}
+          >
+            {category.name}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
